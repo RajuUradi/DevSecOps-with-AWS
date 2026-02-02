8598f71c23f5f758cd2f6bbcc30802e040437f85
RajuUradi <rajuuradi75@gmail.com>
2026-02-02 11:39:12 +0530

    changes made

---
+++ Shell_Scripting/variables/Variables1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# conversation between Suresh and Ramesh 
+
+echo "Ramesh : Hi Suresh ,How are you ?"
+echo "Suresh : Hi  Ramesh ,Iam fine .What about you ?"
+echo "Ramesh : Iam doing well ."
+echo "Suresh : Nice to meet you again ."
