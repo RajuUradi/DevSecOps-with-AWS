663d999ea1cc3a57efa2d029592becfe26170dae
RajuUradi <rajuuradi75@gmail.com>
2026-02-02 11:30:31 +0530

    added variables inside shell_scripting dir

---
+++ Shell_Scripting/Variables.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Ramesh : Hi Suresh ,How are you ?"
+echo "Suresh : Hi  Ramesh ,Iam fine .What about you ?"
+echo "Ramesh : Iam doing well ."
+echo "Suresh : Nice to meet you again ."
