033c3cb49a342cb1f4e35ab3ecc0bab4ebc99166
RajuUradi <rajuuradi75@gmail.com>
2026-02-02 12:17:36 +0530

    file added

---
+++ Shell_Scripting/variables/Variables2.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+Person1=Raju
+Person2=Kishore
+
+echo "$Person1 : Hi $Person2 ,How are you ?"
+echo "$Person2 : Hi $Person1 , Iam fine and how are you ?"
+echo "$Person1 : Iam good"
+echo "$Person2 : well fine Alright"
+
