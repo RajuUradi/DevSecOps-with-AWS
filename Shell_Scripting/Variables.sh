f98f52d334e88737f44e821f83dff7364ab35259
RajuUradi <rajuuradi75@gmail.com>
2026-02-02 11:41:13 +0530

    remove variable file

--- Shell_Scripting/Variables.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-echo "Ramesh : Hi Suresh ,How are you ?"
-echo "Suresh : Hi  Ramesh ,Iam fine .What about you ?"
-echo "Ramesh : Iam doing well ."
-echo "Suresh : Nice to meet you again ."
