df325e0aba78080257a9731510bd608833f63fc6
RajuUradi <rajuuradi75@gmail.com>
2026-02-03 10:33:25 +0530

    var3 passing arguments

---
+++ Shell_Scripting/variables/Variables3.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# passing arguments 
+
+echo "this is first var $1 and this is second var $2"
+
+#while running scrit in gitbash/linuxserver need to pass two args ex: sh variables.3 Raju Kishore
+#0/p: this is first var Raju and this is second var Kishore
