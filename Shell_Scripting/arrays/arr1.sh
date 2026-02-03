f6e195c10af7517a64aa43333fbb92869ff7671b
RajuUradi <rajuuradi75@gmail.com>
2026-02-03 11:44:26 +0530

    changes made

---
+++ Shell_Scripting/arrays/arr1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+arr=(raju ajay )
+echo ${arr[0]}
+echo ${arr[1]}
+
+echo ${arr[@]} # prints all array values
+echo ${#arr[@]} # length of array
+
+arr+=(kishore) # adding an element in arr
+echo ${arr[2]} 
+
+for i in ${arr[@]}
+do 
+ echo " getting from for loop : $i"
+ done
