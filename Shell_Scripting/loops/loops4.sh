e236cf53780f6788db01bd9c8bfefa37d3b31910
RajuUradi <rajuuradi75@gmail.com>
2026-02-11 10:12:20 +0530

    changes

---
+++ Shell_Scripting/loops/loops4.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+#install packages if they are not yet installed in my server 
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ];then
+  echo "please use root user access"
+  exit 1
+  else
+  echo "iam a root user"
+fi
+
+for i in $@
+do
+ dnf list installed $i
+ if [ $? -ne 0] ; then
+    echo installing $1
+    dnf install $i -y  # pass args as nginx ,nodejs ..
+    echo "INSTALLING $i IS SUCCESSFULL"
+ else
+   echo "$i is already installed"
+ fi   
+done 
+
+echo "No of args passed : $#  and passed args : $@  "
